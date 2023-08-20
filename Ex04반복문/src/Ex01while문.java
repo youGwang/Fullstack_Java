7fd8c341311b0f19bbb901e5622d61e598b0ae1f
서유광 <130361687+youGwang@users.noreply.github.com>
2023-08-20 23:06:16 +0900

    Update Ex01while문.java

--- Ex04반복문/src/Ex01while문.java
+++ Ex04반복문/src/Ex01while문.java
@@ -7,7 +7,7 @@ public class Ex01while문 {
 		
 		System.out.println(1); 
 		System.out.println(2);
-		System.out.println(3);
+		System.out.println(3);  
 		System.out.println(4);
 		System.out.println(5);
 		
